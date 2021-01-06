d1e5b21f9610434dd15e49de8e36514032648316
great084 <great084@gmail.com>
2021-01-06 16:01:42 +0900

    モンスターをランダムで出現させる

--- main.rb
+++ main.rb
@@ -30,15 +30,23 @@ class Monster < Character
 end
 
 class Battle
+  Monster_list = [
+    {name: "スライム", hp: 100, offense: 10, defense: 10},
+    {name: "はぐれメタル", hp: 200, offense: 20, defense: 80},
+    {name: "キメラ", hp: 150, offense: 50, defense: 30},
+    {name: "ゴーレム", hp: 500, offense: 10, defense: 50},
+  ]
+
+
   def initialize
     @brave = Brave.new(name: "ゆうしゃ", hp: 100, offense: 50, defense: 10)
-    @monster = Monster.new(name: "スライム", hp: 130, offense: 50, defense: 10)
+    @monster = Monster.new(Monster_list.sample)
   end
   
   def start
     while battle_continue? do
       print_status
-      sleep(2)
+      sleep(1.5)
       @brave.attack(@monster)
       break unless battle_continue?
       @monster.attack(@brave)
